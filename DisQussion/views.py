0abaeca680e3da7b0bcba42795b9ecaedfeb5960
Yoga <y-o-g-a@gmx.de>
2012-05-28 18:12:28 +0200

    changed views.py

--- DisQussion/views.py
+++ DisQussion/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
-    return render_to_response("Node/Show.html",{"pagename":"Root", "id":1, "slots":[["name":"GP","text":"Gibts noch nicht"],["name":"WP","text":"Inhalte statt Knoten"]]})
+    return render_to_response("node/show.html", {"pagename":"Root", "id":1, "slots":[{"name":"GP", "text":"Gibts noch nicht"}, {"name":"WP", "text":"Inhalte statt Knoten"}]})
