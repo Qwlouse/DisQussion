fdd8d42ef0377460229d662e178bc8743371508a
Qwlouse <klaus.greff@gmx.net>
2012-05-29 15:47:20 +0200

    Wrote a path_helpers module to turn paths into db-objects.

---
+++ DisQussion/structure/path_helpers.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import division, print_function, unicode_literals
+
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+from models import StructureNode
+
+
+def splitComponent(component):
+    """
+    Split a component of the form 'AAA.12' into a pair ('AAA', 12).
+    If no number exists, i.e. the path is 'AAAA' return ('AAAA', -1).
+    May throw an ValueError if the path is not well formed.
+    """
+    s = component.split('.', 1)
+    if len(s) == 1:
+        return s[0], -1
+    else:
+        return s[0], int(s[1])
+
+def splitPath(path):
+    """
+    Split a path of the form 'AAA.123/BBBB.12/CC.1234/' into pairs of (short_title, nr_in_parent).
+    """
+    components = path.strip('/').split('/')
+    return [splitComponent(c) for c in components]
+
+def getSlot(structure_node, slot_name):
+    """
+    Returns the slot with the given name of the given structure_node.
+    Raises ObjectDoesNotExist exception if slot is not found.
+    Raises MultipleObjectsReturned exception if more than one slot is found.
+    """
+    matching_slot_nodes = structure_node.slot_set.filter(short_title=slot_name)
+    if len(matching_slot_nodes) < 1:
+        raise ObjectDoesNotExist("Error: Slot with name '%s' does not exist in StructureNode '%s'"%(slot_name, structure_node))
+    if len(matching_slot_nodes) > 1:
+        raise MultipleObjectsReturned("Error: StructureNode '%s' contains two or more Slots with name '%s'"%(structure_node, slot_name))
+    return matching_slot_nodes[0]
+
+def getChildWithNr(slot, nr):
+    """
+    Return the child node (either TextNode or StructureNode) with nr_in_parent == nr.
+
+    """
+    matching_structure_nodes = slot.structurenode_set.filter(nr_in_parent=nr)
+    if len(matching_structure_nodes) > 1:
+        raise MultipleObjectsReturned("Error: Slot '%s' contains two or more Children with nr '%d'"%(slot, nr))
+    if len(matching_structure_nodes) == 1:
+        return matching_structure_nodes[0]
+
+    matching_text_nodes = slot.textnode_set.filter(nr_in_parent=nr)
+    if len(matching_text_nodes) < 1:
+        raise ObjectDoesNotExist("Error: Slot '%s' does not contain a child with nr %d"%(slot, nr))
+    if len(matching_text_nodes) > 1:
+        raise MultipleObjectsReturned("Error: Slot '%s' contains two or more Children with nr '%d'"%(slot, nr))
+    return matching_text_nodes[0]
+
+
+
+
+def getNodeForPath(path):
+    """
+    Use a path of the form 'AAA.123/BBBB.12/CC.1234/' to get the corresponding text- or structureNode from the DB.
+    """
+    address = splitPath(path)
+    root = StructureNode.objects.filter(parent=None)[0]
+    current_node = root
+    for short_title, nr_in_parent in address:
+        slot = getSlot(current_node, short_title)
+        current_node = getChildWithNr(slot, nr_in_parent)
+
+    return current_node
--- DisQussion/views.py
+++ DisQussion/views.py
@@ -1,30 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.forms import AuthenticationForm
-from DisQussion.structure.models import *
+
+from DisQussion.structure.path_helpers import getNodeForPath
+
 
 def home(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
     return render_to_response("node/show.html",
-            {"pagename":"Root",
+            {"pagename": "Root",
              "form": AuthenticationForm(),
-             "id":1, "slots":[
-                {"name":"GP", "list":[{"id":2, "text":"Gibts noch nicht"}, {"id":3, "text":"Gibbet wohl"}]},
-                {"name":"WP", "list":[{"id":7, "text":"WP is doof"}, {"id":8, "text":"WP is toll"}]}]},
+             "id": 1, "slots": [
+                {"name": "GP", "list": [{"id": 2, "text": "Gibts noch nicht"}, {"id": 3, "text": "Gibbet wohl"}]},
+                {"name": "WP", "list": [{"id": 7, "text": "WP is doof"}, {"id": 8, "text": "WP is toll"}]}]},
         context_instance=RequestContext(request))
-    
+
+
+
+
 def path(request, path):
-        current_node = StructureNode.objects.filter(parent=None)[0] # root
-        for p in path.split("/"):
-            short_title, local_id = p.split(".")
-            slots = current_node.slot_set.filter(short_title=short_title)
-            if len(slots) < 1:
-                    return HttpResponse("Error: Slot not found")
-            slot = slots[0]
-                    
-            
-            text_node = slot.textnode_set.filter(nr_in_parent=local_id).first()
-            text_node = slot.textnode_set.filter(nr_in_parent=local_id).first()
-            
-        return HttpResponse(str(path))
+    return HttpResponse(str(getNodeForPath(path).id))
+
