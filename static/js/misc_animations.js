a2b5d11355023a7d65ba392242bcf44ef266abf3
pinae <pinae@web.de>
2012-07-16 16:26:06 +0200

    Refactored text structure in main.html and changed css and js accordingly

--- static/js/misc_animations.js
+++ static/js/misc_animations.js
@@ -35,72 +35,41 @@ function showlogin_step() {
 }
 
 function hideText() {
-    document.getElementById("hauptText").firstChild.nextSibling.style.opacity = "1.0";
+    document.getElementById("text").style.opacity = "1.0";
     hideText_step();
 }
 
 function hideText_step() {
     //alert(document.getElementById("hauptText").childNodes[1].style.opacity);
-    var opac = parseFloat(document.getElementById("hauptText").childNodes[1].style.opacity);
-    var i;
-    for (i = 0; i < document.getElementById("hauptText").childNodes.length; i++) {
-        if (document.getElementById("hauptText").childNodes[i].nodeType == 1) {
-            document.getElementById("hauptText").childNodes[i].style.opacity = "" + (opac - 0.11);
-        }
-    }
+    var opac = parseFloat(document.getElementById("text").style.opacity);
+    document.getElementById("text").style.opacity = "" + (opac - 0.11);
     if (opac >= 0.01) {
         setTimeout("hideText_step()", 25);
     } else {
-        for (i = 0; i < document.getElementById("hauptText").childNodes.length; i++) {
-            if (document.getElementById("hauptText").childNodes[i].nodeType == 1) {
-                document.getElementById("hauptText").childNodes[i].style.display = "none";
-            }
-        }
+        document.getElementById("text").childNodes[i].style.display = "none";
     }
 }
 
 function showText(sourceNode) {
-    document.getElementById("hauptText").textSource = sourceNode;
+    document.getElementById("text").textSource = sourceNode;
     hideText();
     setTimeout("showText_intermission()", 425);
 }
 
 function showText_intermission() {
-    var textHTML = document.getElementById("hauptText").textSource.textPart;
-    var i, j;
-    for (i = 0; i < document.getElementById("hauptText").childNodes.length; i++) {
-        if (document.getElementById("hauptText").childNodes[i].nodeType == 1) {
-            for (j = 0; j < document.getElementById("hauptText").childNodes[i].attributes.length; j++) {
-                if (document.getElementById("hauptText").childNodes[i].attributes[j].name == "class") {
-                    if (document.getElementById("hauptText").childNodes[i].attributes[j].value == "text") {
-                        document.getElementById("hauptText").childNodes[i].innerHTML = textHTML;
-                    }
-                }
-            }
-        }
-    }
+    var textHTML = document.getElementById("text").textSource.textPart;
+    document.getElementById("text").innerHTML = textHTML;
     showText_step();
 }
 
 function showText_step() {
-    var i, j;
-    for (i = 0; i < document.getElementById("hauptText").childNodes.length; i++) {
-        if (document.getElementById("hauptText").childNodes[i].nodeType == 1) {
-            for (j = 0; j < document.getElementById("hauptText").childNodes[i].attributes.length; j++) {
-                if (document.getElementById("hauptText").childNodes[i].attributes[j].name == "class") {
-                    if (document.getElementById("hauptText").childNodes[i].attributes[j].value == "text") {
-                        document.getElementById("hauptText").childNodes[i].style.display = "block";
-                        var opac = parseFloat(document.getElementById("hauptText").childNodes[i].style.opacity);
-                        document.getElementById("hauptText").childNodes[i].style.opacity = "" + (opac + 0.11);
-                        if (opac < 0.9) {
-                            setTimeout("showText_step()", 25);
-                        } else {
-                            document.getElementById("hauptText").childNodes[i].style.opacity = "1.0";
-                        }
-                    }
-                }
-            }
-        }
+    document.getElementById("text").style.display = "block";
+    var opac = parseFloat(document.getElementById("text").style.opacity);
+    document.getElementById("text").style.opacity = "" + (opac + 0.11);
+    if (opac < 0.9) {
+        setTimeout("showText_step()", 25);
+    } else {
+        document.getElementById("text").style.opacity = "1.0";
     }
 }
 
