0e16e99edc0ed3e8d4b07090067ebc76557a3ae6
Qwlouse <klaus.greff@gmx.net>
2012-07-16 17:40:21 +0200

    Added correct text display for TextNode, SlotNode and StructureNodes.

--- structure/ajax.py
+++ structure/ajax.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
-from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
 from django.template.loader import render_to_string
-from structure.models import Node, TextNode, Slot, StructureNode
+from structure.models import TextNode, Slot, StructureNode
 import json
 
 
-@dajaxice_register
-def test(request):
-    print("Test wurde aufgerufen.")
-    dajax = Dajax()
-    dajax.assign('#textPart','innerHTML','<h1>TEST</h1>')
-    return dajax.json()
-
-@dajaxice_register
-def getNodeText(request, node_id):
-    print('#'+node_id)
-    dajax = Dajax()
-    dajax.assign('#'+node_id, 'textPart', render_to_string('node/renderSlotText.html',
-            {'slot_name': "Wahlprogramm", 'alternatives': [
-                {'id': "1", 'text':"Ist wichtig"},
-                {'id': "2", 'text':"Ist Orange"},
-                {'id': "3", 'text':"Ist voll unnötig"}
-                ]}))
-    return dajax.json()
+def getNodeText(node):
+    if isinstance(node, TextNode):
+        return render_to_string('node/renderTextNode.html',
+            {'title' : node.getShortTitle(),
+             'text'  : node.getText()})
+    elif isinstance(node, Slot):
+        return render_to_string('node/renderSlot.html',
+            {'title' : node.getShortTitle(),
+             'alternatives' : [{'id' : a.nr_in_parent(), 'text' : a.getText()} for a in node.node_set.all()]})
+    elif isinstance(node, StructureNode):
+        return render_to_string('node/renderStructureNode.html',
+            {'title' : node.getShortTitle(),
+             'slots' : [{'short_title' : s.getShortTitle(), 'text' : s.getText()} for s in node.slot_set.all()]})
+    else :
+        return ""
 
 @dajaxice_register
 def getNodeInfo(request, node_id, node_type):
@@ -39,7 +35,7 @@ def getNodeInfo(request, node_id, node_type):
         children = [c.as_leaf_class() for c in node.node_set.all()]
     elif isinstance(node, StructureNode):
         children = node.slot_set.all()
-    return json.dumps({'text' : node.getText(),
+    return json.dumps({'text' : getNodeText(node),
                        'type' : node.getType(),
                        'short_title' : node.getShortTitle(),
                        'id' : node.id,
