1ac964829e9847bc3e3eefb37c3ad4d2398d406b
Qwlouse <klaus.greff@gmx.net>
2012-07-30 16:24:06 +0200

    Added auto-upvote to proposal creation.

--- structure/views.py
+++ structure/views.py
@@ -2,13 +2,23 @@
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
 from django.http import HttpResponseRedirect
-from structure.models import TextNode, Slot
+from structure.models import TextNode, Slot, Vote
+
+def add_auto_upvote(user, text):
+    auto_upvote = Vote()
+    auto_upvote.consent = 1
+    auto_upvote.wording = 1
+    auto_upvote.user = user
+    auto_upvote.text = text
+    auto_upvote.save()
+
 
 def submit_textNode(request):
     t = TextNode()
     t.text = request.POST['text']
     t.parent_id = int(request.POST['slot_id'])
     t.save()
+    add_auto_upvote(request.user, t)
     return HttpResponseRedirect("/" + t.getTextPath())
 
 
@@ -31,4 +41,5 @@ def submit_slot_with_text(request):
     t.text = request.POST['text']
     t.parent = s
     t.save()
+    add_auto_upvote(request.user, t)
     return HttpResponseRedirect("/" + t.getTextPath())
