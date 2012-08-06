a1aa9c773c75e7ac50040aeae5bea65730dc1792
pinae <pinae@web.de>
2012-08-06 18:51:09 +0200

    changed data from getHistory

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -2,15 +2,15 @@ function updateNavigation(data) {
     var navigationNode = document.getElementById('navbar');
     var graphNode = document.getElementById('graph');
     var outerList = document.createElement('ol');
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data['history'].length; i++) {
         var outerListElement = document.createElement('li');
         var innerList = document.createElement('ol');
-        for (var j = 0; j < data[i].length; j++) {
+        for (var j = 0; j < data['history'][i].length; j++) {
             var innerListElement = document.createElement('li');
             var link = document.createElement('a');
-            link.setAttribute('href',data[i][j]['path']);
-            link.setAttribute('onclick',"navigateTo(event,"+data[i][j]['id']+");");
-            var title = document.createTextNode(data[i][j]['title']);
+            link.setAttribute('href',data['history'][i][j]['path']);
+            link.setAttribute('onclick',"navigateTo(event,"+data['history'][i][j]['id']+");");
+            var title = document.createTextNode(data['history'][i][j]['title']);
             link.appendChild(title);
             innerListElement.appendChild(link);
             innerList.appendChild(innerListElement);
@@ -21,10 +21,22 @@ function updateNavigation(data) {
     outerListElement = document.createElement('li');
     outerListElement.appendChild(graphNode);
     outerList.appendChild(outerListElement);
-    //alert("xxx76364736.");
+    outerListElement = document.createElement('li');
+    innerList = document.createElement('ol');
+    for (i = 0; i < data['slot_list'].length; i++) {
+        innerListElement = document.createElement('li');
+        link = document.createElement('a');
+        link.setAttribute('href', data['slot_list'][i]['path']);
+        link.setAttribute('onclick', "navigateTo(event," + data['slot_list'][i]['id'] + ");");
+        title = document.createTextNode(data['slot_list'][i]['title']);
+        link.appendChild(title);
+        innerListElement.appendChild(link);
+        innerList.appendChild(innerListElement);
+    }
+    outerListElement.appendChild(innerList);
+    outerList.appendChild(outerListElement);
     while ( navigationNode.firstChild ) navigationNode.removeChild( navigationNode.firstChild );
     navigationNode.appendChild(outerList);
-    //alert("ganuralischowuraka.");
 }
 
 function navigateTo(event, id) {
