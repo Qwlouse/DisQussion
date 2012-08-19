b5c83090d2e9a7fcdf9effcc5e3be9b3bf50d52b
Qwlouse <klaus.greff@gmx.net>
2012-08-19 20:56:13 +0200

    added non-interactive automatic  creation of a superuser when calling syncdb

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,10 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.conf import settings
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.management import create_superuser
+from django.db.models import signals
 
 class UserProfile(models.Model):
     # This field is required.
@@ -17,4 +20,31 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-post_save.connect(create_user_profile, sender=User)
+signals.post_save.connect(create_user_profile, sender=User)
+
+
+# From http://stackoverflow.com/questions/1466827/ --
+#
+# Prevent interactive question about wanting a superuser created.
+signals.post_syncdb.disconnect(
+    create_superuser,
+    sender=auth_models,
+    dispatch_uid='django.contrib.auth.management.create_superuser')
+
+
+# Create our own test user automatically.
+def create_testuser(app, created_models, verbosity, **kwargs):
+    if not settings.DEBUG:
+        return
+    try:
+        auth_models.User.objects.get(username='admin')
+    except auth_models.User.DoesNotExist:
+        print '*' * 80
+        print 'Creating admin -- login: admin, password: 1234'
+        print '*' * 80
+        assert auth_models.User.objects.create_superuser('admin', 'ab@c.com', '1234')
+    else:
+        print 'Admin user already exists.'
+
+signals.post_syncdb.connect(create_testuser,
+    sender=auth_models, dispatch_uid='common.models.create_testuser')
