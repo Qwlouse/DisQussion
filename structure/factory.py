fe90a6c5ca9d98ecfcdd31fb6ecc44ff7d7a8102
Qwlouse <klaus.greff@gmx.net>
2012-08-19 19:27:07 +0200

    added voting functionality

--- structure/factory.py
+++ structure/factory.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
-from structure.models import TextNode, StructureNode, Slot, Node
+from django.contrib.auth.models import User
+from structure.models import TextNode, StructureNode, Slot, Node, Vote, adjust_vote_caches
 from structure.path_helpers import getRootNode
+from structure.vote_helpers import vote_for_textNode
 
 root = getRootNode()
 nodeDict = {'root' : root}
@@ -66,3 +68,30 @@ def createStructure(parent, slots):
         slot_list = [createSlot(st, s) for s in slots]
     return st, slot_list
 
+def createDummyUsers(cnt):
+    dummy_users_count = User.objects.filter(username__startswith="user_").count()
+    new_users = []
+    for i in range(dummy_users_count, dummy_users_count+cnt):
+        name = "user_" + str(i)
+        new_users.append(User.objects.create_user(name, name + "@server", "password"))
+    return new_users
+
+
+def castVotesFor(text, consent):
+    dummy_users = User.objects.filter(username__startswith="user_")
+    # get/create users that can cast additional votes
+    free_users = list(dummy_users.exclude(vote__text=text))
+    number_of_votes = sum(consent)
+    number_of_free_users = len(free_users)
+    if number_of_free_users < number_of_votes :
+        additional_users = createDummyUsers(number_of_votes - number_of_free_users)
+        free_users += additional_users
+
+    # generate the votes
+    consent_values = [-1] * consent[0]  + [1] * consent[1]
+    for i in range(number_of_votes):
+        vote_for_textNode(free_users[i], text, consent_values[i])
+
+def clearVotesFor(text):
+    Vote.objects.filter(text=text).delete()
+    adjust_vote_caches(text)
