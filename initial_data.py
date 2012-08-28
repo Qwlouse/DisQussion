017489df9faf48b7ebeef344d82ea27b2cb326a4
pinae <pinae@web.de>
2012-08-28 13:47:12 +0200

    initial texts - the beginning

--- initial_data.py
+++ initial_data.py
@@ -13,4 +13,9 @@ def createInitialData():
     gp, gp1 = createSlot('root', "GP", "Kein Grundsatzprogramm vorhanden!")
     wp, wp1 = createSlot('root', "WP", "Inhalte statt Köpfe!")
 
+    #Bildung
+    #textfile = open("initial_data_storage/modul01a.txt", 'r')
+    #modul01a_text = textfile.read()
+    #textfile.close()
+    #bildungsprogramm = createStructure(wp,{"Grundsätze": modul01a_text})
 
