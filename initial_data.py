468ea148a712fb9ba83c303a5b126f7354d7437f
pinae <pinae@web.de>
2012-08-28 14:55:42 +0200

    initial data part 2

--- initial_data.py
+++ initial_data.py
@@ -10,12 +10,50 @@ def createRoot():
 
 def createInitialData():
     populateNodeDict()
-    gp, gp1 = createSlot('root', "GP", "Kein Grundsatzprogramm vorhanden!")
-    wp, wp1 = createSlot('root', "WP", "Inhalte statt Köpfe!")
 
-    #Bildung
-    #textfile = open("initial_data_storage/modul01a.txt", 'r')
-    #modul01a_text = textfile.read()
-    #textfile.close()
-    #bildungsprogramm = createStructure(wp,{"Grundsätze": modul01a_text})
+    bildung = createSlot('root', "Bildung")
+
+    # Wahlprogramm
+    bildung_wp = StructureNode()
+    bildung_wp.parent = lookupNode(bildung, Slot)
+    bildung_wp.save()
+    textfile = open("initial_data_storage/wahlprogramm.txt", 'r')
+    wahlprogramm_text = textfile.read()
+    textfile.close()
+    wp_bildung, wp_bildung_textNode = createSlot(bildung_wp, "Wahlprogramm", wahlprogramm_text)
+
+    # Bildungsstreik Positionspapier
+    bildung_posp = StructureNode()
+    bildung_posp.parent = lookupNode(bildung, Slot)
+    bildung_posp.save()
+    textfile = open("initial_data_storage/bildungsstreik-positionspapier.txt", 'r')
+    bilstr_posp_text = textfile.read()
+    textfile.close()
+    posp, bilstr_posp_textNode = createSlot(bildung_posp, "Bildungsstreik-Posp", bilstr_posp_text)
+
+    # Oktoberkonzept
+    bildung_okt = StructureNode()
+    bildung_okt.parent = lookupNode(bildung, Slot)
+    bildung_okt.save()
+    textfile = open("initial_data_storage/oktoberkonzept.txt", 'r')
+    oktoberkonzept_text = textfile.read()
+    textfile.close()
+    oktoberkonzept_slot, oktoberkonzept_textNode = createSlot(bildung_okt, "Oktoberkonzept", oktoberkonzept_text)
+
+    # Konzept AG Bildung
+    bildung_ag = StructureNode()
+    bildung_ag.parent = lookupNode(bildung, Slot)
+    bildung_ag.save()
+    grundsaetze_slot = createSlot(bildung_ag, "Grundsaetze")
+    grundsaetze = StructureNode()
+    grundsaetze.parent = lookupNode(grundsaetze_slot, Slot)
+    grundsaetze.save()
+    textfile = open("initial_data_storage/modul01a.txt", 'r')
+    modul01a_text = textfile.read()
+    textfile.close()
+    modul01a_slot, modul01a_textNode = createSlot(grundsaetze, "Modul01a", modul01a_text)
+    textfile = open("initial_data_storage/modul01b.txt", 'r')
+    modul01b_text = textfile.read()
+    textfile.close()
+    modul01b_slot, modul01b_textNode = createSlot(grundsaetze, "Modul01b", modul01b_text)
 
