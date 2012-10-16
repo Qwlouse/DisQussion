a0d745889707934d416e2e5aa98b39503a7f6bff
pinae <pinae@web.de>
2012-10-16 22:05:40 +0200

    4 part test

--- structure/tests.py
+++ structure/tests.py
@@ -24,3 +24,24 @@ class StructureParserTest(unittest.TestCase):
         """
         n = parse(s, self.test_slot)
         self.assertEqual(n.getType(), "TextNode")
+
+    def test_tree_headings_results_in_TextNode(self):
+        s = """
+        Introduction
+        ==== Heading 1 ====
+        My name is Harry.
+        === Heading 2 ===
+        I'm quite an idiot.
+        == Heading 3 ==
+        I reversed the ordering of the headings.
+        """
+        titles = ["Einleitung", "Heading_1", "Heading_2", "Heading_3"]
+        textparts = ["Introduction", "My name is Harry.", "I'm quite an idiot.", "I reversed the ordering of the headings."]
+        n = parse(s, self.test_slot)
+        self.assertEqual(n.getType(), "StructureNode")
+        for i, slot in enumerate(n.slot_set.all()):
+            self.assertEqual(slot.getType(), "Slot")
+            print("|"+slot.short_title+"|"+titles[i])
+            self.assertEqual(slot.short_title, titles[i])
+            print("|"+slot.getText()+"|")
+            #self.assertEqual(slot.getText(), textparts[i])
