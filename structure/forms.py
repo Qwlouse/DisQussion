0621374593c9064344e92721d455de68faaccb91
Qwlouse <klaus.greff@gmx.net>
2012-10-29 22:57:26 +0100

    removed unused form

--- structure/forms.py
+++ structure/forms.py
@@ -2,12 +2,6 @@
 # coding=utf-8
 from django import forms
 
-class CreateTextForm(forms.Form):
-    shortTitle = forms.CharField(max_length=20,
-        label="Kurztitel (20 Zeichen)",
-        help_text="Kuztitel, der als Tag verwendet und im Graphen angezeigt wird. Der Kurztitel darf maximal 20 Zeichen lang sein.")
-    text = forms.CharField(widget=forms.Textarea)
-
 CONSENT_CHOICES = ((-1, 'Ablehnung'), (0, 'Enthaltung'), (1,  'Zustimmung'))
 WORDING_CHOICES = ((-1, 'Unlesbar'), (0, 'Enthaltung'), (1,  'Lesenswert'))
 
