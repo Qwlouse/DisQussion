98cd175b02ab7d9c716cdc2f6eaf510ca2d5fbaf
Qwlouse <klaus.greff@gmx.net>
2012-11-06 23:25:59 +0100

    added position calculation

--- static/js/form_magic.js
+++ static/js/form_magic.js
@@ -2,6 +2,12 @@
  * @param frm The VotingForm used to vote for this node
  *
  */
+
+var vote_points = [[[49, 69], [62, 55], [76, 42]],
+                   [[35, 55], [49, 41], [62, 28]],
+                   [[22, 41], [35, 29], [49, 14]]];
+
+
 function submit_vote_for_text_node(frm) {
     var consent = 0;
     for (var i = 0; i < frm.consent.length; i++) {
@@ -42,6 +48,52 @@ function relMouseCoords(event){
 }
 HTMLDivElement.prototype.relMouseCoords = relMouseCoords;
 
+function calculate_coordinates(consent, wording){
+    var p1 = vote_points[1][1];
+    var p2, p3, cons_v, word_v, x, y;
+    if (Math.abs(consent) > Math.abs(wording)) {
+        if (consent >= 0) {
+            p3 = vote_points[2][1];
+            if (wording >= 0) {
+                p2 = vote_points[2][2];
+            } else {
+                p2 = vote_points[2][0];
+            }
+        } else {
+            p3 = vote_points[0][1];
+            if (wording >= 0) {
+                p2 = vote_points[0][2];
+
+            } else {
+                p2 = vote_points[0][0];
+            }
+        }
+        cons_v = [p3[0] - p1[0], p3[1] - p1[1]];
+        word_v = [p2[0] - p3[0], p2[1] - p3[1]];
+    } else {
+        if (wording >= 0) {
+            p3 = vote_points[1][2];
+            if (consent >= 0) {
+                p2 = vote_points[2][2];
+            } else {
+                p2 = vote_points[0][2];
+            }
+        } else {
+            p3 = vote_points[1][0];
+            if (consent >= 0) {
+                p2 = vote_points[2][0];
+            } else {
+                p2 = vote_points[0][0];
+            }
+        }
+        cons_v = [p2[0] - p3[0], p2[1] - p3[1]];
+        word_v = [p3[0] - p1[0], p3[1] - p1[1]];
+    }
+    x = p1[0] + Math.abs(consent)*cons_v[0] + Math.abs(wording)*word_v[0];
+    y = p1[1] + Math.abs(consent)*cons_v[1] + Math.abs(wording)*word_v[1];
+    return [x, y];
+}
+
 function process_vote(subEvent, vote_field, is_structure_node, db_id) {
     var mainEvent = subEvent ? subEvent : window.event;
     var coords = vote_field.relMouseCoords(mainEvent);
@@ -77,9 +129,9 @@ function updateVoting(data) {
         dot.style.borderRadius = "3px";
         dot.style.border = "1px solid #AF0000";
         dot.style.position = "relative";
-        var positions = {0:[50, 70], 1:[36, 56], 3:[64, 56], 2:[23, 43], 4:[50, 43], 6:[77, 43], 5:[36, 29], 7:[64, 29], 8:[50, 16]};
-        dot.style.top = positions[(3 * (Math.round(data["wording"]) + 1) + Math.round(data["consent"]) + 1)][1] - 6 + "px";
-        dot.style.left = positions[(3 * (Math.round(data["wording"]) + 1) + Math.round(data["consent"]) + 1)][0] - 5 + "px";
+        var pos = calculate_coordinates(data["consent"], data["wording"]);
+        dot.style.top = pos[1] - 6 + "px";
+        dot.style.left = pos[0] - 5 + "px";
         dot.style.backgroundColor = "red";
         vote_field.appendChild(dot);
     }
