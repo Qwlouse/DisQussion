fe60714033137eed09e6726b55ce808a9f9843b5
pinae <pinae@web.de>
2012-11-13 22:39:23 +0100

    deactivated transaction middleware for submitting mocroblog entries

--- microblogging/views.py
+++ microblogging/views.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 from django.http import HttpResponseRedirect
+from django.db import transaction
 from microblogging.models import create_entry
 
+@transaction.autocommit
 def submit_Microblog_Entry(request):
     p = create_entry(request.POST['text'], request.user)
     return HttpResponseRedirect(request.POST['post_redirect'])
