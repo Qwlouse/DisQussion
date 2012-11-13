950b902fab0fb45943e67e1dfb4b8914e3a07da5
pinae <pinae@web.de>
2012-11-13 23:45:26 +0100

    deactivated caching middleware

--- microblogging/views.py
+++ microblogging/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponseRedirect
 from django.db import transaction
 from microblogging.models import create_entry
 
-@transaction.autocommit
 def submit_Microblog_Entry(request):
     p = create_entry(request.POST['text'], request.user)
     return HttpResponseRedirect(request.POST['post_redirect'])
